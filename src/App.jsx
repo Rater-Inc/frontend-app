337ef0c58505d91d20776affc6b842c8c5761936
fehmisener <fehmiisener@gmail.com>
2024-06-08 01:53:31 +0200

    feat: add routings for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+
+import CreateSpacePage from './pages/CreatePage/CreateSpacePage';
+import RatingPage from './pages/RatePage/RatingPage';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<CreateSpacePage />} />
+        <Route path="/rating/:spaceId" element={<RatingPage />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
