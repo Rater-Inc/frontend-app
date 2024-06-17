b959fa60f2df8437fd862aa40b0a174e43f5a617
burakYildirim1 <burak.y190m@gmail.com>
2024-06-17 23:41:19 +0300

    fix: change routing 'rating' to 'spaces'

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <CssBaseline />
       <Routes>
         <Route path="/" element={<CreateSpacePage />} />
-        <Route path="/rating/:spaceId" element={<RatingPage />} />
+        <Route path="/space/:spaceId" element={<RatingPage />} />
       </Routes>
     </ThemeProvider>
   );
